f5a977e4c198dc3b108be1adb25e595266294cae
Pedro Sitorus <pedrositorus0@gmail.com>
2026-01-22 15:29:34 +0700

    Delete Coba.java

--- Coba.java
+++
@@ -1,8 +0,0 @@
-public class Coba {
-    public static void main(String[] args) {
-        System.out.println("Halo Fedora, Java sudah jalan!");
-    }
-}
-    public static void main (String [] args) {
-        System.out.println("hello broo");
-    }
